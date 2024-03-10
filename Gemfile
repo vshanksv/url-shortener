39b9bf95d1703ed84e8c0569c03f4650efa3d05a
ray <vshanksv@gmail.com>
2024-03-10 18:42:00 +0800

    ignore user_id column before removing it

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ gem 'jwt'
 gem 'kaminari'
 gem 'mechanize'
 gem 'sidekiq'
+gem 'strong_migrations'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/short_link_fact.rb
+++ app/models/short_link_fact.rb
@@ -1,4 +1,6 @@
 class ShortLinkFact < ApplicationRecord
+  self.ignored_columns = ["user_id"]
+
   belongs_to :short_link, primary_key: :short_url, foreign_key: :short_url, inverse_of: :short_link_facts
 
   validates :short_url, presence: true
