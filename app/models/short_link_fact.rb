3c07b5a1e3ac47dccc87939ec3f8b96fcbd9fec8
ray <vshanksv@gmail.com>
2024-03-11 15:19:52 +0800

    validate ipv4 and ipv6

--- app/models/short_link_fact.rb
+++ app/models/short_link_fact.rb
@@ -4,5 +4,5 @@ class ShortLinkFact < ApplicationRecord
   belongs_to :short_link, primary_key: :short_url, foreign_key: :short_url, inverse_of: :short_link_facts
 
   validates :short_url, presence: true
-  validates :ip, format: { with: Resolv::IPv4::Regex }
+  validates :ip, format: { with: Resolv::AddressRegex }
 end
