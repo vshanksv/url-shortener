844f52343f8588c1146e804d2dd300fb924ff2fe
ray <vshanksv@gmail.com>
2024-03-11 12:33:38 +0800

    update sidekiq redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/1") }
+  config.redis = { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/1"), password: ENV.fetch("REDIS_PASSWORD", nil) }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/1") }
+  config.redis = { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/1"), password: ENV.fetch("REDIS_PASSWORD", nil) }
 end
