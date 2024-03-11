abc6ead30661d7733b32a781bc2b7e8c03c20812
ray <vshanksv@gmail.com>
2024-03-11 13:30:11 +0800

    fixup! join tables to product usage report instead of depending solely on single table

--- app/sidekiq/impression_job.rb
+++ app/sidekiq/impression_job.rb
@@ -4,6 +4,6 @@ class ImpressionJob
   # Sidekiq's retry mechanism will catch exceptions and retry the jobs regularly within 25 retries (about 21 days)
   def perform(ip, short_url)
     short_link = ShortLink.find_by(short_url:)
-    short_link.short_link_facts.create!(ip:, user_id: short_link.user_id)
+    short_link.short_link_facts.create!(ip:)
   end
 end
